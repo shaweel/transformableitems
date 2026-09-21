66985e19905ccd5bd8ce8185a1f534643fbe8b9c
shaweel <shaweel@proton.me>
2026-09-21 18:24:33 +0200

    feat: v1.2

--- src/main/java/me/shaweel/transformableitems/ModKeybinds.java
+++ src/main/java/me/shaweel/transformableitems/ModKeybinds.java
@@ -1,6 +1,5 @@
 package me.shaweel.transformableitems;
 
-import org.lwjgl.glfw.GLFW;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
 
@@ -9,6 +8,6 @@ public class ModKeybinds {
 
 	public static void initialize() {
 		KeyMapping.Category category = KeyMapping.Category.register(Identifier.fromNamespaceAndPath("transformableitems", "transformableitems"));
-		OPEN_CONFIG_KEYBIND = new KeyMapping("key.transformableitems.open_config", GLFW.GLFW_KEY_F7, category);
+		OPEN_CONFIG_KEYBIND = new KeyMapping("key.transformableitems.open_config", 297, category);
 	}
 }
