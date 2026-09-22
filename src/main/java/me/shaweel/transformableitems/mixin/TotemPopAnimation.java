f5142d556f7e5c1e5a40a264e8a64f383b29ef7c
shaweel <shaweel@proton.me>
2026-09-22 19:27:35 +0200

    refactor: change to ModifyArg

--- src/main/java/me/shaweel/transformableitems/mixin/TotemPopAnimation.java
+++ src/main/java/me/shaweel/transformableitems/mixin/TotemPopAnimation.java
@@ -6,40 +6,72 @@ import net.minecraft.client.renderer.GameRenderer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(GameRenderer.class)
 public class TotemPopAnimation {
-	@ModifyArgs(
+	@ModifyArg(
 		method = "renderItemActivationAnimation",
 		at = @At(
 			value = "INVOKE",
 			target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V"
-		)
+		),
+		index = 0
 	)
-	private void modifyTranslate(Args args) {
+	private float modifyTranslateX(float originalX) {
 		TotemConfig totemConfig = ConfigFile.configData.totemConfig;
-
-		float originalX = args.get(0);
-		float originalY = args.get(1);
-
-		args.set(0, originalX + originalX * totemConfig.xOffset);
-		args.set(1, originalY - originalY * totemConfig.yOffset);
+		return originalX + originalX * totemConfig.xOffset;
 	}
 
-	@ModifyArgs(
+	@ModifyArg(
+		method = "renderItemActivationAnimation",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V"
+		),
+		index = 1
+	)
+	private float modifyTranslateY(float originalY) {
+		TotemConfig totemConfig = ConfigFile.configData.totemConfig;
+		return originalY - originalY * totemConfig.yOffset;
+	}
+
+	@ModifyArg(
 		method = "renderItemActivationAnimation",
 		at = @At(
 			value = "INVOKE",
 			target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V"
-		)
+		),
+		index = 0
 	)
-	private void modifyScale(Args args) {
+	private float modifyScaleX(float originalX) {
 		TotemConfig totemConfig = ConfigFile.configData.totemConfig;
+		return originalX * totemConfig.xScale;
+	}
 
-		args.set(0, (float) args.get(0) * totemConfig.xScale);
-		args.set(1, (float) args.get(1) * totemConfig.yScale);
-		args.set(2, (float) args.get(2) * totemConfig.zScale);
+	@ModifyArg(
+		method = "renderItemActivationAnimation",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V"
+		),
+		index = 1
+	)
+	private float modifyScaleY(float originalY) {
+		TotemConfig totemConfig = ConfigFile.configData.totemConfig;
+		return originalY * totemConfig.yScale;
+	}
+
+	@ModifyArg(
+		method = "renderItemActivationAnimation",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V"
+		),
+		index = 2
+	)
+	private float modifyScaleZ(float originalZ) {
+		TotemConfig totemConfig = ConfigFile.configData.totemConfig;
+		return originalZ * totemConfig.zScale;
 	}
 }
