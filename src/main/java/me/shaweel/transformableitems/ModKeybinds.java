dbdfd1d322258eddf51e16f4b0a5576285e3562a
shaweel <shaweel@proton.me>
2026-09-22 21:45:23 +0200

    fix: wrong default keybind

--- src/main/java/me/shaweel/transformableitems/ModKeybinds.java
+++ src/main/java/me/shaweel/transformableitems/ModKeybinds.java
@@ -1,5 +1,6 @@
 package me.shaweel.transformableitems;
 
+import org.lwjgl.glfw.GLFW;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
 
@@ -8,6 +9,6 @@ public class ModKeybinds {
 
 	public static void initialize() {
 		KeyMapping.Category category = KeyMapping.Category.register(Identifier.fromNamespaceAndPath("transformableitems", "transformableitems"));
-		OPEN_CONFIG_KEYBIND = new KeyMapping("key.transformableitems.open_config", 297, category);
+		OPEN_CONFIG_KEYBIND = new KeyMapping("key.transformableitems.open_config", GLFW.GLFW_KEY_F7, category);
 	}
 }
