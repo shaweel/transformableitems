11f428dc5520ad6577ffacc8d6968d848cfbb73a
shaweel <shaweel@proton.me>
2026-09-22 19:37:26 +0200

    feat: v1.2

--- src/main/java/me/shaweel/transformableitems/ConfigScreen.java
+++ src/main/java/me/shaweel/transformableitems/ConfigScreen.java
@@ -16,9 +16,12 @@ import net.minecraft.util.Mth;
 public class ConfigScreen extends Screen {
 	public enum OptionTypes { FLOAT_SLIDER, BOOLEAN_OPTION }
 	public static final int DONE_BUTTON_PADDING = 7;
-	public static final int TITLE_PADDING = 12;
+	public static final int TITLE_PADDING = 7;
+	public static final int TITLE_TO_TAB_PADDING = 7;
 	public static final int RESET_BUTTON_WIDTH = 50;
 	public static final int DONE_BUTTON_WIDTH = 200;
+	public static final int TAB_WIDTH = 100;
+	public static final int TAB_AMOUNT = 3;
 	public static final int WIDGET_WIDTH = 208;
 	public static final int WIDGET_HEIGHT = 20;
 	public static final int WIDGET_PADDING = 4;
@@ -30,13 +33,15 @@ public class ConfigScreen extends Screen {
 
 	public static final float DEFAULT_SCALE = 1f;
 	public static final float DEFAULT_OFFSET = 0f;
-	
+
+	public int currentTab = 0;
+
 	public ConfigScreen() {
 		super(Component.literal("ConfigScreen"));
 	}
 
 	private int row(int index, int rowAmount) {
-		int headerSpace = TITLE_PADDING + this.font.lineHeight;
+		int headerSpace = TITLE_PADDING + this.font.lineHeight + TITLE_TO_TAB_PADDING + WIDGET_HEIGHT;
 		int footerSpace = DONE_BUTTON_PADDING + WIDGET_HEIGHT;
 
 		int allWidgetsHeight = rowAmount * WIDGET_HEIGHT + (rowAmount - 1) * WIDGET_PADDING;
@@ -47,6 +52,11 @@ public class ConfigScreen extends Screen {
 		return startY + (WIDGET_HEIGHT + WIDGET_PADDING) * index;
 	}
 	
+	private int categoryX(int index) {
+		float left = this.width / 2 - (WIDGET_PADDING + TAB_WIDTH) * ((float) TAB_AMOUNT / 2);
+		return (int) left + (WIDGET_PADDING + TAB_WIDTH) * index;
+	}
+
 	private int x() {
 		return x(WIDGET_WIDTH + WIDGET_PADDING + RESET_BUTTON_WIDTH);
 	}
@@ -65,6 +75,10 @@ public class ConfigScreen extends Screen {
 
 	private void createButton(int x, int y, int w, int h, String name, Runnable action) {
 		this.addRenderableWidget(Button.builder(Component.literal(name), button -> action.run()).bounds(x, y, w, h).build());
+	}
+
+	private void createTabButton(int index, int x, int y, int w, int h, String name, Runnable action) {
+		this.addRenderableWidget(new TabButton(index, x, y, w, h, Component.literal(name), button -> action.run()));
 	}
 
 	private void createSlider(int x, int y, int w, int h, String name, double min, double max, Supplier<Float> getter, Consumer<Float> setter, Float defaultValue) {
@@ -90,7 +104,7 @@ public class ConfigScreen extends Screen {
 			@Override
 			protected void updateMessage() {
 			setMessage(Component.literal(
-				String.format("%s: %.2f", name, denormalize(min, max, this.value))
+				String.format("%s: %.2f", name, Math.round(denormalize(min, max, this.value) * 100f) / 100f)
 			));
 			}
 
@@ -122,38 +136,107 @@ public class ConfigScreen extends Screen {
 		this.addRenderableWidget(stringWidget);
 	}
 
+	private void switchTab(int index) {
+		currentTab = index;
+
+		clearWidgets();
+		init();
+	}
+
+	private void createTab(int index, String name) {
+		createTabButton(index, categoryX(index), TITLE_TO_TAB_PADDING + TITLE_PADDING + this.font.lineHeight, TAB_WIDTH, WIDGET_HEIGHT, name, () -> {
+			switchTab(index);
+		});
+	}
+
+	private void initNormalTab() {
+		createSlider(x(), row(0, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "X Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.normalConfig.xScale, value -> ConfigFile.configData.normalConfig.xScale = value, DEFAULT_SCALE);
+
+		createSlider(x(), row(1, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.normalConfig.yScale, value -> ConfigFile.configData.normalConfig.yScale = value, DEFAULT_SCALE);
+
+		createSlider(x(), row(2, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Z Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.normalConfig.zScale, value -> ConfigFile.configData.normalConfig.zScale = value, DEFAULT_SCALE);
+
+
+		createSlider(x(), row(3, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "X Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.normalConfig.xOffset, value -> ConfigFile.configData.normalConfig.xOffset = value, DEFAULT_OFFSET);
+
+		createSlider(x(), row(4, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.normalConfig.yOffset, value -> ConfigFile.configData.normalConfig.yOffset = value, DEFAULT_OFFSET);
+
+		createSlider(x(), row(5, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Z Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.normalConfig.zOffset, value -> ConfigFile.configData.normalConfig.zOffset = value, DEFAULT_OFFSET);
+
+		createBooleanOption(x(), row(6, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Item Height Animations",
+		() -> ConfigFile.configData.normalConfig.itemHeightAnimations, value -> ConfigFile.configData.normalConfig.itemHeightAnimations = value, true);
+	}
+
+	private void initFoodTab() {
+		createSlider(x(), row(0, 6), WIDGET_WIDTH, WIDGET_HEIGHT, "X Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.foodConfig.xScale, value -> ConfigFile.configData.foodConfig.xScale = value, DEFAULT_SCALE);
+
+		createSlider(x(), row(1, 6), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.foodConfig.yScale, value -> ConfigFile.configData.foodConfig.yScale = value, DEFAULT_SCALE);
+
+		createSlider(x(), row(2, 6), WIDGET_WIDTH, WIDGET_HEIGHT, "Z Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.foodConfig.zScale, value -> ConfigFile.configData.foodConfig.zScale = value, DEFAULT_SCALE);
+
+
+		createSlider(x(), row(3, 6), WIDGET_WIDTH, WIDGET_HEIGHT, "X Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.foodConfig.xOffset, value -> ConfigFile.configData.foodConfig.xOffset = value, DEFAULT_OFFSET);
+
+		createSlider(x(), row(4, 6), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.foodConfig.yOffset, value -> ConfigFile.configData.foodConfig.yOffset = value, DEFAULT_OFFSET);
+
+		createSlider(x(), row(5, 6), WIDGET_WIDTH, WIDGET_HEIGHT, "Z Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.foodConfig.zOffset, value -> ConfigFile.configData.foodConfig.zOffset = value, DEFAULT_OFFSET);
+	}
+
+	private void initTotemTab() {
+		createSlider(x(), row(0, 5), WIDGET_WIDTH, WIDGET_HEIGHT, "X Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.totemConfig.xScale, value -> ConfigFile.configData.totemConfig.xScale = value, DEFAULT_SCALE);
+
+		createSlider(x(), row(1, 5), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.totemConfig.yScale, value -> ConfigFile.configData.totemConfig.yScale = value, DEFAULT_SCALE);
+
+		createSlider(x(), row(2, 5), WIDGET_WIDTH, WIDGET_HEIGHT, "Z Scale", MIN_SCALE, MAX_SCALE, 
+		() -> ConfigFile.configData.totemConfig.zScale, value -> ConfigFile.configData.totemConfig.zScale = value, DEFAULT_SCALE);
+
+
+		createSlider(x(), row(3, 5), WIDGET_WIDTH, WIDGET_HEIGHT, "X Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.totemConfig.xOffset, value -> ConfigFile.configData.totemConfig.xOffset = value, DEFAULT_OFFSET);
+
+		createSlider(x(), row(4, 5), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Offset", MIN_OFFSET, MAX_OFFSET,
+		() -> ConfigFile.configData.totemConfig.yOffset, value -> ConfigFile.configData.totemConfig.yOffset = value, DEFAULT_OFFSET);
+	}
+
+	private void initTab() {
+		if (currentTab == 0) {
+			initNormalTab();
+		} else if (currentTab == 1) {
+			initFoodTab();
+		} else if (currentTab == 2) {
+			initTotemTab();
+		}
+	}
+
 	@Override
 	protected void init() {
 		super.init();
 
 		createText(x(this.font.width("Transformable Items Configuration")), TITLE_PADDING, "Transformable Items Configuration");
 
-		createSlider(x(), row(0, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "X Scale", MIN_SCALE, MAX_SCALE, 
-		() -> ConfigFile.configData.xScale, value -> ConfigFile.configData.xScale = value, DEFAULT_SCALE);
+		createTab(0, "Normal");
+		createTab(1, "Eating Animation");
+		createTab(2, "Totem Animation");
 
-		createSlider(x(), row(1, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Scale", MIN_SCALE, MAX_SCALE, 
-		() -> ConfigFile.configData.yScale, value -> ConfigFile.configData.yScale = value, DEFAULT_SCALE);
-
-		createSlider(x(), row(2, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Z Scale", MIN_SCALE, MAX_SCALE, 
-		() -> ConfigFile.configData.zScale, value -> ConfigFile.configData.zScale = value, DEFAULT_SCALE);
-
-
-		createSlider(x(), row(3, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "X Offset", MIN_OFFSET, MAX_OFFSET,
-		() -> ConfigFile.configData.xOffset, value -> ConfigFile.configData.xOffset = value, DEFAULT_OFFSET);
-
-		createSlider(x(), row(4, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Y Offset", MIN_OFFSET, MAX_OFFSET,
-		() -> ConfigFile.configData.yOffset, value -> ConfigFile.configData.yOffset = value, DEFAULT_OFFSET);
-
-		createSlider(x(), row(5, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Z Offset", MIN_OFFSET, MAX_OFFSET,
-		() -> ConfigFile.configData.zOffset, value -> ConfigFile.configData.zOffset = value, DEFAULT_OFFSET);
-
-
-		createBooleanOption(x(), row(6, 7), WIDGET_WIDTH, WIDGET_HEIGHT, "Item Height Animations",
-		() -> ConfigFile.configData.itemHeightAnimations, value -> ConfigFile.configData.itemHeightAnimations = value, true);
+		initTab();
 
 		createButton(x(DONE_BUTTON_WIDTH), this.height - DONE_BUTTON_PADDING - WIDGET_HEIGHT, DONE_BUTTON_WIDTH, WIDGET_HEIGHT, "Done", this::onClose);
 	}
-
+	
 	@Override
 	public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
 		this.renderBackground(poseStack);
