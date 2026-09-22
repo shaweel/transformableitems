0272fb7176fa9106adaead83c5002c7cba1443e8
shaweel <shaweel@proton.me>
2026-09-22 06:14:06 +0200

    feat: v1.2

---
+++ src/main/java/me/shaweel/transformableitems/mixin/TotemPopAnimation.java
@@ -0,0 +1,43 @@
+package me.shaweel.transformableitems.mixin;
+
+import me.shaweel.transformableitems.ConfigFile;
+import me.shaweel.transformableitems.ConfigFile.TabConfig;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+
+@Mixin(GameRenderer.class)
+public class TotemPopAnimation {
+	@Inject(method = "renderItemActivationAnimation", at = @At("HEAD"))
+	private void transform(GuiGraphics guiGraphics, float f, CallbackInfo callbackInfo) {
+		TabConfig tabConfig = ConfigFile.configData.get(2);
+
+		double divisor = 1.3;
+
+		guiGraphics.pose().translate(
+			tabConfig.xOffset / divisor,
+			tabConfig.yOffset / divisor,
+			tabConfig.zOffset / divisor
+		);
+	}
+	@ModifyArgs(
+		method = "renderItemActivationAnimation",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V"
+		)
+	)
+	private void modifyScale(Args args) {
+		TabConfig tabConfig = ConfigFile.configData.get(2);
+
+		args.set(0, (float) args.get(0) * tabConfig.xScale);
+		args.set(1, (float) args.get(1) * tabConfig.yScale);
+		args.set(2, (float) args.get(2) * tabConfig.zScale);
+	}
+}
