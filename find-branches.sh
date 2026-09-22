a39b2b2e39b6fc1f02b5407f91cd4e7d620ab96e
shaweel <shaweel@proton.me>
2026-09-22 21:48:31 +0200

    remove redundant file

--- find-branches.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/); do
-    if git grep -q '297' "$branch" -- '**/ModKeybinds.java' 2>/dev/null; then
-        echo "$branch"
-    fi
-done
