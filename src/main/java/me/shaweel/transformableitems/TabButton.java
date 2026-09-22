c84396c2d3b7b35318788099834d55adecfb32a5
shaweel <shaweel@proton.me>
2026-09-22 18:58:49 +0200

    feat: v1.2

---
+++ src/main/java/me/shaweel/transformableitems/TabButton.java
@@ -0,0 +1,23 @@
+package me.shaweel.transformableitems;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.Component;
+
+public class TabButton extends Button {
+	private final int index;
+
+	public TabButton(int index, int x, int y, int width, int height, Component message, OnPress onPress) {
+		super(x, y, width, height, message, onPress, DEFAULT_NARRATION);
+		this.index = index;
+	}
+
+	@Override 
+	public boolean isHoveredOrFocused() {
+		if (!(Minecraft.getInstance().screen instanceof ConfigScreen configScreen)) {
+			return false;
+		}
+
+		return configScreen.currentTab == index;
+	}
+}
