9c3bf841a18c94c3e98699bb9c6a6b906f58f38e
shaweel <shaweel@proton.me>
2026-09-24 07:19:18 +0200

    feat: v1.2

---
+++ src/main/java/me/shaweel/transformableitems/CustomItemRenderer.java
@@ -0,0 +1,78 @@
+package me.shaweel.transformableitems;
+
+import me.shaweel.transformableitems.ConfigFile.NormalOrFoodConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemStack;
+
+public class CustomItemRenderer extends ItemRenderer {
+	public CustomItemRenderer(Minecraft client) {
+		super(client);
+	}
+	
+	private boolean isEating(EntityLivingBase livingEntity) {
+		return livingEntity.isHandActive() && livingEntity.getActiveItemStack().getItemUseAction() == EnumAction.EAT;
+	}
+
+	@Override
+	public void renderItemSide(
+		EntityLivingBase livingEntity,
+		ItemStack itemStack,
+		TransformType transformType,
+		boolean leftHanded
+	) {
+		NormalOrFoodConfig tabConfig = isEating(livingEntity) ? ConfigFile.configData.foodConfig : ConfigFile.configData.normalConfig;
+
+		GlStateManager.pushMatrix();
+
+		if (transformType == TransformType.FIRST_PERSON_LEFT_HAND) {
+			GlStateManager.translate(
+				tabConfig.xOffset,
+				tabConfig.yOffset,
+				tabConfig.zOffset
+			);
+
+			GlStateManager.scale(
+				tabConfig.xScale,
+				tabConfig.yScale,
+				tabConfig.zScale
+			);
+
+		} else if (transformType == TransformType.FIRST_PERSON_RIGHT_HAND) {
+			GlStateManager.translate(
+				tabConfig.xOffset * -1,
+				tabConfig.yOffset,
+				tabConfig.zOffset
+			);
+
+			GlStateManager.scale(
+				tabConfig.xScale,
+				tabConfig.yScale,
+				tabConfig.zScale
+			);
+		}
+
+		super.renderItemSide(livingEntity, itemStack, transformType, leftHanded);
+
+		GlStateManager.popMatrix();
+	}
+
+	@Override
+	public void updateEquippedItem() {
+		super.updateEquippedItem();
+
+		if (ConfigFile.configData.normalConfig.itemHeightAnimations) return;
+
+		this.equippedProgressMainHand = 1f;
+		this.equippedProgressOffHand = 1f;
+		this.prevEquippedProgressMainHand = 1f;
+		this.prevEquippedProgressOffHand = 1f;
+		if (Minecraft.getMinecraft().player == null) return;
+		this.itemStackMainHand = Minecraft.getMinecraft().player.getHeldItemMainhand();
+		this.itemStackOffHand = Minecraft.getMinecraft().player.getHeldItemOffhand();
+	}
+}
--- src/main/java/me/shaweel/transformableitems/TransformableItemsInitializer.java
+++ src/main/java/me/shaweel/transformableitems/TransformableItemsInitializer.java
@@ -1,15 +1,33 @@
 package me.shaweel.transformableitems;
 
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = "transformableitems", guiFactory = "me.shaweel.transformableitems.ConfigScreenFactory")
 public class TransformableItemsInitializer {
+	private static boolean replaced = false;
+
+	@SubscribeEvent
+	public void postInit(TickEvent.ClientTickEvent event) {
+		if (event.phase != TickEvent.Phase.END || replaced) {
+			return;
+		}
+		
+		Minecraft mc = Minecraft.getMinecraft();
+		
+		mc.entityRenderer = new CustomEntityRenderer(mc, mc.getResourceManager());
+		mc.entityRenderer.itemRenderer = new CustomItemRenderer(mc);
+
+		replaced = true;
+	}
+	
 	public TransformableItemsInitializer() {
 		ConfigFile.load();
 		MinecraftForge.EVENT_BUS.register(ModKeybinds.class);
-		MinecraftForge.EVENT_BUS.register(ItemHeightAnimations.class);
-		MinecraftForge.EVENT_BUS.register(ModelBake.class);
+		MinecraftForge.EVENT_BUS.register(this);
 		ModKeybinds.initialize();
 	}
 }
